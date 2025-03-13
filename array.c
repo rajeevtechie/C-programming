07fe7e379294860c28c9731e99599478ad80a749
rajeevtechie <rajeev.techie380@gmail.com>
2025-03-13 10:13:46 +0530

    practise some basics of array

--- array.c
+++ array.c
@@ -3,32 +3,32 @@ void numbers(int arr[],int n);
 int main()
 {
     float age[5]={45,65,23,15,56};
-    // float marks[4];
-    // printf("marks of maths: ");
-    // scanf("%f",&marks[0]);
+    float marks[4];
+    printf("marks of maths: ");
+    scanf("%f",&marks[0]);
 
-    // printf("marks of physics: ");
-    // scanf("%f",&marks[1]);
+    printf("marks of physics: ");
+    scanf("%f",&marks[1]);
 
-    // printf("marks of chemistry: ");
-    // scanf("%f",&marks[2]);
+    printf("marks of chemistry: ");
+    scanf("%f",&marks[2]);
 
-    // printf("maths=%f \nphysics=%f \nchemistry=%f\n",marks[0],marks[1],marks[2]);
+    printf("maths=%f \nphysics=%f \nchemistry=%f\n",marks[0],marks[1],marks[2]);
 
     //Qn: WAP to enter the price of 3 items and print their final cost with gst.
-    // float price[3];
-    // printf("Enter the price of first item: ");
-    // scanf("%f",&price[0]);
-    // printf("Enter the price of second item: ");
-    // scanf("%f",&price[1]);
-    // printf("Enter the price of third item: ");
-    // scanf("%f",&price[2]);
-    // float sum=price[0]+price[1]+price[2];
-    // float final_cost = sum+sum*(18/100);
-    // printf("the final cost with gst is: %f\n",final_cost);
+    float price[3];
+    printf("Enter the price of first item: ");
+    scanf("%f",&price[0]);
+    printf("Enter the price of second item: ");
+    scanf("%f",&price[1]);
+    printf("Enter the price of third item: ");
+    scanf("%f",&price[2]);
+    float sum=price[0]+price[1]+price[2];
+    float final_cost = sum+sum*(18/100);
+    printf("the final cost with gst is: %f\n",final_cost);
 
-    // int arr[]={1,2,3,4,5,6};
-    // numbers(arr,6); //calling the function.
+    int arr[]={1,2,3,4,5,6};
+    numbers(arr,6); //calling the function.
 
     //multidimensional array: 
     int m[2][3];  // _ _ _
